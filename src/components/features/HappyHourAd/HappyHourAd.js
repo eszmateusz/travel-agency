785a37cf0b19ae8ae2089867b4365ec952e569c8
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2021-02-07 14:29:16 +0100

    [20.2] Add test and HappyHourAd component in TDD. Change Hero component.

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './HappyHourAd.scss';
+import PropTypes from 'prop-types';
+
+class HappyHourAd extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    title: PropTypes.string,
+    promoDescription: PropTypes.string,
+  };
+
+  static defaultProps = {
+    title: 'Happy Hour',
+    promoDescription: 'Its Happy Hour! All offers 20% off!',
+  };
+
+  componentDidMount(){
+    /* run this.forceUpdate() every second */
+    this.timerID = setInterval(() => 
+      this.forceUpdate(), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  getCountdownTime() {
+    const currentTime = new Date();
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(),
+      currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+  
+    if(currentTime.getUTCHours() >= 12) {
+      nextNoon.setUTCDate(currentTime.getUTCDate()+1);
+    }
+  
+    return Math.round((nextNoon.getTime() - currentTime.getTime())/1000);
+  }
+
+  render() {
+    const { title, promoDescription } = this.props;
+    const countDownTime = this.getCountdownTime();
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.promoDescription}>
+          {countDownTime > 82800 ? promoDescription : countDownTime }</div>
+      </div>
+    );
+  }
+}
+
+export default HappyHourAd;
