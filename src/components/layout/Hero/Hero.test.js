73aba0718366f2394f7b9b903fe40cd8d307ef4e
Mateusz Szymonik <mateusz.szymonik@gmail.com>
2021-03-04 15:59:49 +0100

    [20.4] Add daysToSummer.

--- src/components/layout/Hero/Hero.test.js
+++ src/components/layout/Hero/Hero.test.js
@@ -5,6 +5,7 @@ import Hero from './Hero';
 describe('Component Hero', () => {
   it('should render without crashing', () => {
     const component = shallow(<Hero titleText='Lorem ipsum' imageSrc='image.jpg' />);
+
     expect(component).toBeTruthy();
   });
 
@@ -17,6 +18,7 @@ describe('Component Hero', () => {
     const expectedImage = 'image.jpg';
     const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
     const renderedTitle = component.find('.title').text();
+
     expect(renderedTitle).toEqual(expectedTitle);
     expect(component.find('.image').prop('src')).toEqual(expectedImage);
   });
@@ -24,6 +26,7 @@ describe('Component Hero', () => {
   it('renders correct classNames', () => {
     const mockVariants = 'small dummy';
     const component = shallow(<Hero titleText='Lorem' imageSrc='image.jpg' variant={mockVariants} />);
+
     expect(component.hasClass('component')).toBe(true);
     expect(component.hasClass('small')).toBe(true);
     expect(component.hasClass('dummy')).toBe(true);
@@ -35,5 +38,13 @@ describe('Component Hero', () => {
     const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
 
     expect(component.find('HappyHourAd').length).toEqual(1);
+  });
+
+  it('should render DaysToSummer', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<Hero titleText={expectedTitle} imageSrc={expectedImage} />);
+
+    expect(component.find('DaysToSummer').length).toEqual(1);
   });
 });
